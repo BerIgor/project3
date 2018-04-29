f37aeb48c6b45bbdf86020b2289a62d1c4977314
BerIgor <bigor11@gmail.com>
2018-04-29 14:42:19 +0300

    Updated gitignore
    removed idea folder

---
+++ src/basic_tobii.py
@@ -0,0 +1,13 @@
+from pygaze import libscreen
+from pygaze import libtime
+from pygaze import liblog
+from pygaze import libinput
+from pygaze import eyetracker
+
+print("Hello World")
+
+# # # # #
+# experiment setup
+
+# create display object
+screen = libscreen.Screen()
